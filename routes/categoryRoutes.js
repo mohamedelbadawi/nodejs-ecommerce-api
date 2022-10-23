2256e8c821beef97a22175d750ea503491ae48ff
mohamedelbadawi <redamohamed619@gmail.com>
2022-10-23 17:20:58 +0100

    category crud operation

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
-const { getCategories } = require('../services/CategoryServices');
-router.get('/', getCategories);
+const { getCategories, createCategory, getCategory, updateCategory, deleteCategory } = require('../services/CategoryServices');
+const { createCategoryValidator, getCategoryValidator, updateCategoryValidator, deleteCategoryValidator } = require('../utils/validators/CategoryValidator');
+router.route('/').post(createCategoryValidator, createCategory).get(getCategories);
+router.route('/:id').get(getCategoryValidator, getCategory).put(updateCategoryValidator, updateCategory).delete(deleteCategoryValidator, deleteCategory);
 module.exports = router;
