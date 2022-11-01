12c9e119d9db76bd8d77774314bfb5c52fc916ea
mohamedelbadawi <redamohamed619@gmail.com>
2022-11-01 15:15:33 +0200

    user permission and roles

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     password_updated_at: Date,
     role: {
         type: String,
-        enum: ["user", "admin"],
+        enum: ["user", "supervisor", "admin"],
         default: "user"
     },
     is_active: { type: Boolean, default: true }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const { allowedTo, auth } = require('../middlewares/TokenHandler');
 const { updateUser, getUser, deleteUser, getUsers, createUser, uploadUserImage, resizeUserImage, updateUserPassword } = require('../services/UserServices');
 const { createUserValidator, getUserValidator, updateUserValidator, deleteUserValidator, updateUserPasswordValidator } = require('../utils/validators/UserValidator');
 
-router.route('/').post(uploadUserImage, resizeUserImage, createUserValidator, createUser).get(getUsers);
-router.route('/:id').get(getUserValidator, getUser).put(updateUserValidator, updateUser).delete(deleteUserValidator, deleteUser);
+router.route('/').post(auth, allowedTo('admin'), uploadUserImage, resizeUserImage, createUserValidator, createUser).get(getUsers);
+router.route('/:id').get(getUserValidator, getUser).put(auth, allowedTo('admin'), updateUserValidator, updateUser).delete(auth, allowedTo('admin'), deleteUserValidator, deleteUser);
 router.put('/changePassword/:id', updateUserPasswordValidator, updateUserPassword)
 
 module.exports = router;
